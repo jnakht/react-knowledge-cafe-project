b84fc47064ad5f4f16842ea5f74eafcc6d0123f0
jnakht <jisan.cse.cu@gmail.com>
2025-02-19 13:52:27 +0600

    header added, blogs json file added, data loaded

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Components/Header/Header'
+import Blogs from './Components/Blogs/Blogs'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h3 className='text-3xl text-red-600'>React Knowledge Cafe</h3>
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h3 className='text-[#111] text-4xl font-bold '>React Knowledge Cafe</h3>
+        </div>
+    );
+};
+
+export default Header;
