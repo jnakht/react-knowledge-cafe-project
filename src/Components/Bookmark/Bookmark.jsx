d096d53808a93ed3f7caf90a05307c275d5b8edb
jnakht <jisan.cse.cu@gmail.com>
2025-02-20 00:14:27 +0600

    bookmarks half style done

--- src/Components/Bookmark/Bookmark.jsx
+++ src/Components/Bookmark/Bookmark.jsx
@@ -3,8 +3,8 @@
 const Bookmark = ({bookmark}) => {
     console.log('bookmark set hoise')
     return (
-        <div>
-            <p>{bookmark}</p>
+        <div className="bg-white p-5 w-[80%] mx-auto rounded-xl">
+            <p className="text-[#111] text-lg font-semibold">{bookmark}</p>
         </div>
     );
 };
--- src/Components/Bookmarks/Bookmarks.jsx
+++ src/Components/Bookmarks/Bookmarks.jsx
@@ -4,14 +4,17 @@ import Bookmark from "../Bookmark/Bookmark";
 const Bookmarks = ({bookmarks}) => {
     return (
         <div className="border-2 border-green-600 w-[29%]">
-            <div>
+            <div className="h-[80px]">
                 {/* spent time code here */}
+                Spent time on read : 177 min
             </div>
-            <div>
-                <h3>Bookmarked Blogs : {bookmarks.length}</h3>
+            <div className=" bg-[#1111110D]">
+                <h3 className="text-[#111] text-2xl font-bold">Bookmarked Blogs : {bookmarks.length}</h3>
+            <div className="flex flex-col gap-4">
             {
                 bookmarks.map(bookmark => <Bookmark bookmark={bookmark}></Bookmark>)
             }
+            </div>
             </div>
         </div>
     );
